568b09caa9abfb314e71e05e5c9d656b171d1503
andheller <aheller9@gmail.com>
2022-06-18 11:06:06 -0500

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import * as esbuild from 'esbuild-wasm';
+
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+		});
+	};
+</script>
+
+<h1>esbuild wasm in browser test</h1>
